4041a32320a2278cc1d49660121f6d5792476a94
Rei-Jones <reiavi7r@gmail.com>
2025-08-14 22:15:10 -0500

    fixed errors

--- Machine_Learning/group_events.py
+++ Machine_Learning/group_events.py
@@ -15,6 +15,7 @@ import h5py
 import sys
 import re
 import numpy as np
+import pandas as pd
 
 #config
 
@@ -33,28 +34,36 @@ def get_percent(folder):
 
 for event_type in os.listdir(sim_data_base):
     type_path = os.path.join(sim_data_base, event_type)
-    if os.path.isdir(type_path) and event_type == "0nubb" or event_type == "leptoquark": #only accept those 2 event types
+
+    if os.path.isdir(type_path) and event_type in ["0nubb", "leptoquark"]:
+
         for pressure in os.listdir(type_path):
             pressure_path = os.path.join(type_path, pressure)
+
             if os.path.isdir(pressure_path) and "bar" in pressure_path:
+
                 for smear in os.listdir(pressure_path):
                     smear_path = os.path.join(pressure_path, smear)
                     percent = get_percent(smear)
+
                     for file in os.listdir(smear_path):
-                        if ".h5" in file:
+
+                        if file.endswith(".h5"):
+
                             file_path = os.path.join(smear_path, file)
-                            with h5py.File(file_path, "r") as f:
-                                hits_table = f["MC/hits/table"]
-                                for event in hits_table["index"]:
-                                    hits_raw = hits_table["values_block_0"][event][:10]
-                                    hits_xyz = np.array([hit[:3] for hit in hits_raw])
-                                    grouped_events[str(event)] = {
-                                        "path": file_path,
-                                        "type": event_type,
-                                        "pressure": pressure,
-                                        "diffusion": percent,
-                                        "hits": hits_xyz.to_list()
-                                    }
+                            hits_df = pd.read_hdf(file_path, "MC/hits")
+
+                            #Loop through each event in this file
+                            for event_id, group in hits_df.groupby("event_id"):
+                                hits_xyz = group[["x", "y", "z"]].to_numpy()
+
+                                grouped_events[str(event_id)] = {
+                                    "path": file_path,
+                                    "type": event_type,
+                                    "pressure": pressure,
+                                    "diffusion": percent,
+                                    "hits": hits_xyz.tolist()
+                                }
 
 #event_#_type_pressure_percent
 
@@ -77,7 +86,7 @@ with open (json_file, "r") as f:
                 and pressure_from_vertex == pressure_from_grouped 
                 and percent_from_vertex == percent_from_grouped):
                 for dim, loc in info.items():
-                    grouped_events[key][dim] = [loc]
+                    grouped_events[key][dim] = [float(loc)]
 
 with open ("events_with_vertex.json", "w") as f:
     json.dump(grouped_events, f, indent=2)
